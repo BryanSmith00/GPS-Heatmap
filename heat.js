c363fce101f7d2c121f0e146e3d7ce48e47cb0cf
Bryan Smith <46660845+BryanSmith00@users.noreply.github.com>
2024-03-05 18:32:44 -0600

    test

--- heat.js
+++ heat.js
@@ -1,24 +1,12 @@
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
-ctx.fillStyle = "green";
-// Add a rectangle at (10, 10) with size 100x100 pixels
-ctx.fillRect(10, 10, 100, 100);
+var map = L.map("map").setView([-37.87, 175.475], 12);
 
-ctx.beginPath();
-ctx.arc(110, 110, 50, 0, Math.PI * 2);
-ctx.stroke();
-
-var map = L.map("map").setView([51.505, -0.09], 13);
-
-var circle = L.circle([51.508, -0.11], {
-  color: "red",
-  fillColor: "#f03",
-  fillOpacity: 0.5,
-  radius: 500,
-}).addTo(map);
-
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 20,
+var tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
+
+addressPoints = addressPoints.map(function (p) {
+  return [p[0], p[1]];
+});
+
+var heat = L.heatLayer(addressPoints).addTo(map);
--- heatmap.py
+++ heatmap.py
@@ -6,7 +6,6 @@ import parsefile
 
 data = []
 
-#parsefile.parse_gpx("C:/Users/bryan/Documents/programming/gpx-heatmap/tracks/2023-09-01 09_17_14.gpx")
 start_time = time.time()
 
 for file in os.listdir("C:/Users/Bryan/Documents/Programming/gpx-heatmap/tracks"):
@@ -15,16 +14,16 @@ for file in os.listdir("C:/Users/Bryan/Documents/Programming/gpx-heatmap/tracks"
 
 m = folium.Map(prefer_canvas=True)
 
-# for point in data[200000:400000]:
-#     folium.Circle(
-#         location=point,
-#         radius=15,
-#         color="cornflowerblue",
-#         stroke=False,
-#         fill=True,
-#         fill_opacity=0.25,
-#         opacity=1,
-#     ).add_to(m)
+for point in data[200000:400000]:
+    folium.Circle(
+        location=point,
+        radius=15,
+        color="cornflowerblue",
+        stroke=False,
+        fill=True,
+        fill_opacity=0.25,
+        opacity=1,
+    ).add_to(m)
 
 HeatMap(data, radius=15, gradient={0.60: 'blue', 0.7: 'green', 0.80: 'yellow', 0.9: 'orange', 1: 'red'}).add_to(m)
 
